63e9515768ec182cab63963af4059abefba2eeda
Canned-pineapple-8 <nastentsiya.filimonova@mail.ru>
2025-11-12 17:23:05 +0300

    Refactor Process.py

--- Process.py
+++ Process.py
@@ -3,6 +3,8 @@ from Memory import Memory
 from RandomFactory import RandomFactory
 from Command import *
 from typing import *
+from dataclasses import dataclass
+
 
 class ProcessState(Enum):
     NEW = 0  # только создан ("загружается")
@@ -16,10 +18,35 @@ class ProcessState(Enum):
     STOPPED = 8  # приостановлен
 
 
+@dataclass
+class ProcessMemoryConfig:
+    block_start: int = -1
+    block_size: int = -1
+    operands_block_address: int = -1
+    result_block_address: int = -1
+
+
+@dataclass
+class ProcessStatistics:
+    total_commands_counter: int = 0
+    io_commands_counter: int = 0
+
+
+@dataclass
+class ProcessCommandsConfig:
+    regular_commands_cnt: int = 10
+    io_command_ratio: float = 0.5
+    min_operand: int = 1
+    max_operand: int = 10
+
+
 class Process:
     free_pid = 0
 
-    def __init__(self, ph_memory_ptr: Memory, memory: int = 10, regular_commands_size: int = 10, io_commands_percentage: float = 0.5) -> None:
+    def __init__(self, ph_memory_ptr: Memory, process_memory_info: ProcessMemoryConfig = ProcessMemoryConfig(),
+                 process_statistics: ProcessStatistics = ProcessStatistics(),
+                 process_commands_config: ProcessCommandsConfig = ProcessCommandsConfig()
+                 ) -> None:
         """
         Инициализация процесса
         :param pid: идентификатор процесса (целое неотрицательное число)
@@ -28,45 +55,36 @@ class Process:
         self.memory_ptr = ph_memory_ptr
         self.pid = Process.free_pid  # свободное значение PID Для новых процессов
         Process.free_pid += 1
-        self.memory = memory
-        self.commands_size = regular_commands_size
-        self.io_commands_percentage = io_commands_percentage
+        self.current_state = ProcessState.NEW
 
-        self._current_state = ProcessState.NEW
+        self.process_memory_config = process_memory_info
+        self.process_statistics = process_statistics
+        self.process_commands_config = process_commands_config
 
-        self.commands_counter = 0
-        self.io_commands_counter = 0
-
-        self.current_command: Optional[Command] = None
-
-        self.command_result_address = -1
-        self.block_start_address = -1
-        return
-
-    @property
-    def current_state(self) -> ProcessState:
-        return self._current_state
-
-    @current_state.setter
-    def current_state(self, state) -> None:
-        if not isinstance(state, ProcessState):
-            raise ValueError("Неверное состояние процесса")
-        self._current_state = state
+        self.current_command = None
         return
 
     def generate_command(self) -> Command:
-        if self.commands_size - self.commands_counter <= 0:
+        commands_size = self.process_commands_config.regular_commands_cnt
+        io_commands_percentage = self.process_commands_config.io_command_ratio
+        commands_counter = self.process_statistics.total_commands_counter
+        operands_address = self.process_memory_config.operands_block_address
+
+        if commands_size - commands_counter <= 0:
             self.current_command = ExitCommand()
+            return self.current_command
+
         percent = RandomFactory.generate_random_float_value(0.0, 1.0)
-        if percent < self.io_commands_percentage:
+        if percent < io_commands_percentage:
             io_command_length = RandomFactory.generate_random_int_value(1, 2)
             self.current_command = IOCommand(io_command_length)
         else:
-            op_1_address = self.block_start_address
-            op_2_address = self.block_start_address + 1
+            min_operand, max_operand = self.process_commands_config.min_operand, self.process_commands_config.max_operand
+            op_1_address = operands_address
+            op_2_address = operands_address + 1
 
-            op_1 = RandomFactory.generate_random_int_value(1, 10)
-            op_2 = RandomFactory.generate_random_int_value(1, 10)
+            op_1 = RandomFactory.generate_random_int_value(min_operand, max_operand)
+            op_2 = RandomFactory.generate_random_int_value(min_operand, max_operand)
             self.memory_ptr.write(op_1, op_1_address)
             self.memory_ptr.write(op_2, op_2_address)
 
